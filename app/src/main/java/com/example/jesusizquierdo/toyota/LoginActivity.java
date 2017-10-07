d7935e74ab6e0258a4fb28ba9c46d73abf95fbd6
Jesus Izquierdo <izquierdo843@gmail.com>
2017-10-07 04:46:54 -0400

    facebook worked

--- app/src/main/java/com/example/jesusizquierdo/toyota/LoginActivity.java
+++ app/src/main/java/com/example/jesusizquierdo/toyota/LoginActivity.java
@@ -228,7 +228,7 @@ public class LoginActivity extends AppCompatActivity  {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+//this is a small change
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
