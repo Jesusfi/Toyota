cce904a43aeabb1d12316da8796df5fa28402612
Jesus Izquierdo <izquierdo843@gmail.com>
2017-10-07 02:05:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jesusizquierdo/toyota/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.jesusizquierdo.toyota;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+
+        Button signOut = (Button) findViewById(R.id.btn_sign_out);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
