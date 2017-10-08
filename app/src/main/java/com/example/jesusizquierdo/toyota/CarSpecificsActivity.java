35952486a5469f726816723dbc787ffde7ef4b63
Jesus Izquierdo <izquierdo843@gmail.com>
2017-10-07 23:02:14 -0400

    waitigng for firebase ui recycleview

--- app/src/main/java/com/example/jesusizquierdo/toyota/CarSpecificsActivity.java
+++ app/src/main/java/com/example/jesusizquierdo/toyota/CarSpecificsActivity.java
@@ -138,5 +138,6 @@ public class CarSpecificsActivity extends AppCompatActivity {
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("builds").child(user.getUid()).push();
         reference.setValue(car);
         //this is a change real quick
+        // change database
     }
 }
