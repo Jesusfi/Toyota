cd50b20df46bcd7d514b1714b85641381efe33cb
Jesus Izquierdo <izquierdo843@gmail.com>
2017-10-08 05:29:45 -0400

    sharebutton kinda there

--- app/src/main/java/com/example/jesusizquierdo/toyota/rvadapters/FirebaseCarViewHolder.java
+++ app/src/main/java/com/example/jesusizquierdo/toyota/rvadapters/FirebaseCarViewHolder.java
@@ -2,7 +2,11 @@ package com.example.jesusizquierdo.toyota.rvadapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +16,11 @@ import com.example.jesusizquierdo.toyota.CarListActivity;
 import com.example.jesusizquierdo.toyota.Nfc;
 import com.example.jesusizquierdo.toyota.R;
 import com.example.jesusizquierdo.toyota.classes.Car;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static java.security.AccessController.getContext;
 
 /**
  * Created by Jesus Izquierdo on 10/7/2017.
@@ -34,7 +43,22 @@ public class FirebaseCarViewHolder extends RecyclerView.ViewHolder implements Vi
         final TextView carPackage = (TextView) itemView.findViewById(R.id.tvf_package_firebaseui);
         final ImageView carImage = (ImageView) itemView.findViewById(R.id.imageView_for_firebaseui);
         Button shareNFC = (Button) itemView.findViewById(R.id.btn_share_nfc);
+        Button shareSocial = (Button) itemView.findViewById(R.id.button_share_social);
 
+        shareSocial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String sharebody = "body";
+                String shareSub = "sub here";
+                intent.putExtra(Intent.EXTRA_SUBJECT, sharebody);
+                intent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                ((CarListActivity)mContext).startActivity(Intent.createChooser(intent,"Share using"));
+
+
+            }
+        });
         shareNFC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
