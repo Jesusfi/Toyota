5260780e195e06eee1018ce1a5575c22c692c675
Jesus Izquierdo <izquierdo843@gmail.com>
2017-10-07 23:22:09 -0400

    commit before dinner

--- app/src/main/java/com/example/jesusizquierdo/toyota/rvadapters/FirebaseCarViewHolder.java
+++ app/src/main/java/com/example/jesusizquierdo/toyota/rvadapters/FirebaseCarViewHolder.java
@@ -1,8 +1,32 @@
 package com.example.jesusizquierdo.toyota.rvadapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.example.jesusizquierdo.toyota.classes.Car;
+
 /**
  * Created by Jesus Izquierdo on 10/7/2017.
  */
 
-public class FirebaseCarViewHolder {
+public class FirebaseCarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    View mView;
+    Context mContext;
+
+    public FirebaseCarViewHolder(View itemView) {
+        super(itemView);
+        this.mContext = itemView.getContext();
+        this.mView = itemView;
+        itemView.setOnClickListener(this);
+    }
+    public void bindCar(Car car){
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
